9278a3bb204c4d06c91e6fc5bcfa4016e29a4b6f
Maria <ronios@yandex.ru>
2022-01-09 03:21:15 +0500

    2. Трекер SQL [#1734]

--- src/main/java/ru/job4j/tracker/store/MemTracker.java
+++ src/main/java/ru/job4j/tracker/store/MemTracker.java
@@ -3,9 +3,8 @@ package ru.job4j.tracker.store;
 import ru.job4j.tracker.model.Item;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class MemTracker {
+public class MemTracker implements Store {
 
     private final List<Item> items = new ArrayList<>();
 
